5b0300e8d77bdc8395bf2a5cfabf77d4fa8fe8b2
tuskerlager <194850722+tuskerlager@users.noreply.github.com>
2025-01-15 00:48:09 +0300

    sites without new tabs working, sites with new tabs not working, but webpack is the way.

--- src/content/content.js
+++ src/content/content.js
@@ -1,13 +1,20 @@
-import * as businessDailyAfrica from "./site/businessDailyAfrica";
-import * as theEastAfrican from "./site/theEastAfrican";
-import * as nationAfrica from "./site/nationAfrica";
+// import * as businessDailyAfrica from "./site/businessDailyAfrica";
+// import * as theEastAfrican from "./site/theEastAfrican";
+// import * as nationAfrica from "./site/nationAfrica";
+const businessDailyAfrica = require("./site/businessDailyAfrica");
+const theEastAfrican = require("./site/theEastAfrican");
+const nationAfrica = require("./site/nationAfrica");
 
-const sites = { businessDailyAfrica, theEastAfrican, nationAfrica };
+const sites = [
+  businessDailyAfrica, //
+  theEastAfrican, //
+  nationAfrica, //
+];
 
 // ============= MAIN FUNCTION=============
 function main() {
   const tabHostname = window.location.hostname;
-  console.log("Current hostname: ", hostname); // Debug: log the hostname to check what it returns
+  console.log("Current hostname: ", tabHostname); // Debug: log the hostname to check what it returns
 
   const site = sites.find((site) => site.hostname === tabHostname);
   if (site) {
